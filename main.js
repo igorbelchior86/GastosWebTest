dff166f92a71394e8888c8e77faa6d05dffc7942
igorbelchior86 <61795831+igorbelchior86@users.noreply.github.com>
2025-07-04 14:59:16 -0400

    back to test

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/1
 
 // Flag for mocking data while working on UI.  
 // Switch to `false` to reconnect to production Firebase.
-const USE_MOCK = false;               // produção
+const USE_MOCK = true;                // ambiente de teste
 const APP_VERSION = '1.0.0';
 let save, load;
 let firebaseDb;
